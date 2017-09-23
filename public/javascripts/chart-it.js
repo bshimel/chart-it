dc329ba0ba23d25e9704c8a21501e8f4ff13661c
Bruce <bruce@shimels.com>
2017-09-23 08:42:32 -0500

    Original commit

---
+++ public/javascripts/chart-it.js
@@ -0,0 +1,29 @@
+$(function () {
+
+    var datasource = {
+        'name': 'Lao Lao',
+        'title': 'general manager',
+        'children': [
+            { 'name': 'Bo Miao', 'title': 'department manager' },
+            { 'name': 'Su Miao', 'title': 'department manager',
+                'children': [
+                    { 'name': 'Tie Hua', 'title': 'senior engineer' },
+                    { 'name': 'Hei Hei', 'title': 'senior engineer',
+                        'children': [
+                            { 'name': 'Pang Pang', 'title': 'engineer' },
+                            { 'name': 'Xiang Xiang', 'title': 'UE engineer' }
+                        ]
+                    }
+                ]
+            },
+            { 'name': 'Hong Miao', 'title': 'department manager' },
+            { 'name': 'Chun Miao', 'title': 'department manager' },
+        ]
+    };
+
+    $('#chart-container').orgchart({
+        'data': datasource,
+        'nodeContent': 'title',
+        'toggleSiblingsResp': true
+    });
+});
